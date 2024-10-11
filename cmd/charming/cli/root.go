8873924415964037e9e367ce935dbbb0eb1037e1
r3d5un <r3d5un@pm.me>
2024-10-11 18:47:21 +0200

    Create charming CLI root command

---
+++ cmd/charming/cli/root.go
@@ -0,0 +1,26 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:     "charming",
+	Short:   "CLI tools for learning the charmbracelet stack",
+	Version: "v0.0.1",
+}
+
+func init() {
+	versionTemplate := `{{printf "%s: %s - version %s\n" .Name .Short .Version}}`
+	rootCmd.SetVersionTemplate(versionTemplate)
+}
---
+++ cmd/charming/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/r3d5un/charm/cmd/charming/cli"
+)
+
+func main() {
+	cli.Execute()
+}
